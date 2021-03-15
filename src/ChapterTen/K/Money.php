eafe3a5c2750365fdc0aa184b134d044d7fedfc1
Jeremy Presutti <Jeremy@Presutti.us>
2021-03-14 21:38:33 -0400

    Chapter 10 complete

---
+++ src/ChapterTen/K/Money.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright 2021 Jeremy Presutti <Jeremy@Presutti.us>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
+
+namespace TopTierCode\tddphp\ChapterTen\K;
+
+/**
+ * Class Money - From page 48, second part
+ */
+class Money
+{
+
+    public function __construct(int $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    protected int $amount;
+
+    protected string $currency;
+
+    public function times(int $multiplier): Money
+    {
+        return new Money($this->amount * $multiplier, $this->currency);
+    }
+
+    public function currency(): string
+    {
+        return $this->currency;
+    }
+
+    public function equals(object $object): bool
+    {
+        // This example differs because the type casting in PHP is not the same as java.
+        $money = $object instanceof Money ? $object : null;
+        return $this->amount === $money->amount && $this->currency === $money->currency;
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount, 'USD');
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount, 'CHF');
+    }
+
+    public function __toString(): string
+    {
+        return $this->amount . ' ' . $this->currency;
+    }
+}
---
+++ tests/ChapterTen/E/MoneyTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright 2021 Jeremy Presutti <Jeremy@Presutti.us>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace TopTierCode\tddphp\tests\ChapterTen\E;
+
+use PHPUnit\Framework\TestCase;
+use TopTierCode\tddphp\ChapterTen\E\Money;
+
+/**
+ * Class MoneyTest - From page 39
+ */
+class MoneyTest extends TestCase
+{
+
+    public function testMultiplication(): void
+    {
+        $five = Money::dollar(5);
+        $this->assertEquals(Money::dollar(10), $five->times(2));
+        $this->assertEquals(Money::dollar(15), $five->times(3));
+    }
+
+    public function testEquality(): void
+    {
+        $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
+        $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
+        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
+        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
+        $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
+    }
+
+    /**
+     * @group skip
+     */
+    public function testFrancMultiplication(): void
+    {
+        $five = Money::franc(5);
+        $this->assertEquals(Money::franc(10), $five->times(2));
+        $this->assertEquals(Money::franc(15), $five->times(3));
+    }
+
+    public function testCurrency(): void
+    {
+        $this->assertEquals('USD', Money::dollar(1)->currency());
+        $this->assertEquals('CHF', Money::franc(1)->currency());
+    }
+}
